addf08c667375cf0e6084b958423b3bed7ecc855
mire-web <amahe8664@gmail.com>
2024-08-16 04:40:42 +0100

    lockboxes-solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,7 @@ Author: Mirey-dev(formerly mire-web)
 
 def canUnlockAll(boxes):
     """ Check if all boxes can be unlocked """
-    keys = set([0]) # Store found keys
+    keys = set([0])  # Store found keys
     for idx, box in enumerate(boxes):
         for key in box:
             if key > 0 and key < len(boxes):
@@ -16,4 +16,3 @@ def canUnlockAll(boxes):
         if len(keys) == len(boxes):
             return True
     return False
-
