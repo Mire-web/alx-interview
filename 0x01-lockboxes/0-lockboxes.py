2682fdb480184185d0423eebf85494a13919e099
mire-web <amahe8664@gmail.com>
2024-08-16 04:38:06 +0100

    lockboxes-solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Implementation of the Lockbox challenge solution in Python
+Author: Mirey-dev(formerly mire-web)
+"""
+
+
+def canUnlockAll(boxes):
+    """ Check if all boxes can be unlocked """
+    keys = set([0]) # Store found keys
+    for idx, box in enumerate(boxes):
+        for key in box:
+            if key > 0 and key < len(boxes):
+                if key != idx:
+                    keys.add(key)
+        if len(keys) == len(boxes):
+            return True
+    return False
+
