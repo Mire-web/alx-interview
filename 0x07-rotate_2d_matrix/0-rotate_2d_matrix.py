114b656c38b4db89977ba71f73d77be58e0378e6
mire-web <amahe8664@gmail.com>
2024-11-23 16:11:39 +0100

    rotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,8 +7,12 @@ Author: Mirey_Dev
 
 def rotate_2d_matrix(matrix):
     size = len(matrix) - 1
+    if size < 2:
+        n = 1
+    else:
+        n = size // 2
 
-    for i in range(size // 2):
+    for i in range(n):
         for j in range(i, size-i):
             temp = matrix[size-j][i]
             matrix[size-j][i] = matrix[size-i][size-j]
--- 0x07-rotate_2d_matrix/main.py
+++ 0x07-rotate_2d_matrix/main.py
@@ -9,6 +9,17 @@ if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
+    mat = [[1, 3],
+           [4, 5]]
+    mat3 = [[1, 4, 6, 8],
+            [3, 2, 5, 9],
+            [5, 2, 1, 3],
+            [4, 9, 7, 0]]
 
     rotate_2d_matrix(matrix)
+    rotate_2d_matrix(mat)
+    rotate_2d_matrix(mat3)
+    rotate_2d_matrix([])
     print(matrix)
+    print(mat)
+    print(mat3)
