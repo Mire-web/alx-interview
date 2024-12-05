e53c50466a4ff9c3cd3b212c2d3cedeed22b5b0f
mire-web <amahe8664@gmail.com>
2024-12-05 17:48:55 +0100

    perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Solving the island perimeter challenge in python
+using DFS
+"""
+def island_perimeter(grid):
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(row):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < len(grid) and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1]:
+                    perimeter -= 1
+                if col < len(grid[0]) and col > grid[row][col + 1]:
+                    perimeter -= 1
+    return perimeter * 2
+
